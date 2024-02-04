4164c0ad1c5ec6d84205bf44ba6dc6483bac028c
IsabellaPachec <isabellapacheco404@gmail.com>
2024-02-04 16:05:06 -0300

    Ponteiros

--- Faculdade/seman1.cpp
+++ Faculdade/seman1.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-    std::cout << "Hello, World!" << std::endl;
+    int var1 = 5;
+    int* pont1 = & var1;//o * significa que é um ponteiro o & significa que o ponteiro recebe a variavel 
+    
+
+    
+    cout << "Declarando Ponteiros" << endl;
+    cout << "valor da variavel, atraves de seu numero " << var1 << endl;
+    cout << "valor do ponteiro, atraves de seu numero " << pont1 << endl;
+    cout << "valor do ponteiro, atraves de seu numero " << *pont1 << endl;
+
     return 0;
 }
