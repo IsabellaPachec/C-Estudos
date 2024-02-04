aeaf494d2546370023c829e0833d2308ff8da2d2
IsabellaPachec <isabellapacheco404@gmail.com>
2024-02-04 16:14:02 -0300

    declarando ponteiros

--- Faculdade/seman1.cpp
+++ Faculdade/seman1.cpp
@@ -12,5 +12,22 @@ int main(){
     cout << "valor do ponteiro, atraves de seu numero " << pont1 << endl;
     cout << "valor do ponteiro, atraves de seu numero " << *pont1 << endl;
 
+    int var2;
+    //int var2 = *pont1;
+    //cout << var2;
+
+    // var1 = 30; //mundando valor de uma variavel com ponteiro;
+    //*pont1 = 30; 
+    //cout << var1 << endl;
+
+    var2 = 50; //mudando o valor que o ponteiro aponta.
+    //pont1 = &var2;
+    //cout << *pont1 << endl;
+
+    int* pont2;
+    cout << *pont2 << endl;
+    
+
+
     return 0;
 }
